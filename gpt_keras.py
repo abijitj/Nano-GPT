d1f5dc8da5c0fb9dfe918b2784d1f5403d1c7b16
abijitj <ajayachandran@ucsd.edu>
2024-05-11 11:44:02 -0700

    Add Block and GPTModel classes

--- gpt_keras.py
+++ gpt_keras.py
@@ -15,6 +15,8 @@ dropout = 0.2
 n_layer = 4
 # ------------
 
+
+
 class Head(k.Model): 
     def __init__(self, head_size): 
         super().__init__()
@@ -66,7 +68,76 @@ class FeedForward(k.Model):
         self.l2 = k.layers.Dense(4 * n_embd, n_embd)
         self.dropout = k.layers.Dropout(dropout)
     
-    def forward(self, x): 
+    def call(self, x): 
         x = self.relu(self.l1(n_embd))
         x = self.dropout(self.l2(x))
         return x
+
+
+class Block(k.Model): 
+    """ Transformer block: communication followed by computation """
+
+    def __init__(self, n_embd, n_head): 
+        super().__init__()
+        # n_embd: embedding dimension, n_head: the number of heads we'd like
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+        self.ln1 = k.layers.LayerNormalization()
+        self.ln2 = k.layers.LayerNormalization()
+    
+    def call(self, x): 
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
+    
+
+class GPTModel(k.Model): 
+    def __init__(self):
+        super().__init__()
+        # each token directly reads off the logits for the next token from a lookup table
+        self.token_embedding_table = k.layers.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = k.layers.Embedding(block_size, n_embd)
+        self.block = k.models.Sequential()
+        for _ in range(n_layer): 
+            self.block.add(Block(n_embd, n_head=n_head))
+        self.ln_f = k.layers.LayerNormalization(n_embd) # final layer norm
+        self.lm_head = k.layers.Dense(n_embd, vocab_size)
+    
+    def call(self, idx, targets=None): 
+        B, T = idx.shape
+
+        # idx and targets are both (B,T) tensor of integers
+        tok_emb = self.token_embedding_table(idx) # (B,T,C)
+        pos_emb = self.position_embedding_table(np.arange(T))#, device=device)) # (T,C)
+        x = tok_emb + pos_emb
+
+        x = self.block(x)
+        logits = self.lm_head(tok_emb) # (B,T,vocab_size)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = k.losses.CategoricalCrossentropy()(targets, logits)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -block_size:]
+            # get the predictions
+            logits, loss = self(idx_cond)
+            # focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = k.activations.softmax(logits, dim=-1) # (B, C)
+            # sample from the distribution
+            idx_next = np.random.multinomial(probs, num_samples=1) # (B, 1)
+            # append sampled index to the running sequence
+            idx = k.layers.concatenate((idx, idx_next), dim=1) # (B, T+1)
+        return idx
